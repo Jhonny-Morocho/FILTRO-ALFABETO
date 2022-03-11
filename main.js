e0b3828c28c11a21a91a9ae0f7c2aee76e5cc667
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-03-11 15:41:49 -0500

    tarea terminada

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ var app = new Vue({
     el: '#main',
     data: {
       message: 'Hola Vue!',
-      input:'funeraria jaramillo',
+      input:'',
       vocales:'',
       consonantes:''
     },
     methods: {
         btnEjecutar(){
-            this.vocales=this.input.replace('/[aeiouáéíóú]/','');
+            if(!this.input){
+                alert("Debe ingresar una cadena ");
+            }
+            var strVocal=this.input;
+            var strConsonantes=this.input;
+            this.vocales=strVocal.replace(/[^aáAÁeéEÉiíIÍoOóÓuúUÚ]/g,'');
+            this.consonantes=strConsonantes.replace(/[ aáAÁeéEÉiíIÍoOóÓuúUÚ]/g,'');
         }
     },
 })
